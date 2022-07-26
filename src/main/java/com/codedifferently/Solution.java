548d2d1be54d77896543ab0eb2a0996c6e450557
Anazsa Blanding <anazsa@Anazsas-MacBook-Pro.local>
2022-07-26 09:33:53 -0400

    close enough

--- src/main/java/com/codedifferently/Solution.java
+++ src/main/java/com/codedifferently/Solution.java
@@ -1,7 +1,23 @@
 package com.codedifferently;
 
+import java.util.TreeMap;
+
+
+
 public class Solution {
     public String howManyLettersDoYouSee(String input){
-        return null;
+        TreeMap<Character,Integer> newMap = new TreeMap();
+        char[] toChar = input.toCharArray();
+       // int count =0;
+        for (int i =0; i < toChar.length; i++){
+            newMap.put(toChar[i],1);
+            int count = newMap.get(toChar[i]);
+            if(newMap.containsKey(toChar[i])){
+                count++;
+            }
+            if(count > 0){
+                newMap.put(toChar[i],count+1);
+            }
+        }        return newMap.toString();
     }
 }
